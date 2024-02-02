5c0a46b25c06e0ca8c16a2829b44e0d98278b4cd
NathanGrenier <nathangrenier01@gmail.com>
2024-02-02 12:01:21 -0500

    docs: added assigment pdf to repo

--- Assignment1/test.pl
+++ Assignment1/test.pl
@@ -1,13 +1,2 @@
-% FILEPATH: /code/concordia/SOEN-331/test.pl
-
-% Facts
-likes(john, pizza).
-likes(mary, sushi).
-likes(john, sushi).
-likes(mary, chocolate).
-
-% Rules
-friend(X, Y) :- likes(X, Z), likes(Y, Z).
-
-% Queries
-?- friend(john, mary).
+larger(X, Y, X) :- X > Y, !.
+larger(X, Y, Y).
